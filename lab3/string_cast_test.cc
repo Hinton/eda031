af717a0e30b6698b8f5b78f952569cd9b2f0b387
Hinton <hinton.oscar@gmail.com>
2016-02-14 20:18:36 +0100

    Mostly finished A5.

--- lab3/string_cast_test.cc
+++ lab3/string_cast_test.cc
@@ -1,12 +1,20 @@
+#include <sstream>
 #include <string>
 #include <stdexcept>
 #include "date.h"
 
 using namespace std;
 
-template <typename T> T string_cast(const string& a) {
+template <typename T>
+T string_cast(const string& a) {
+
+	// istream?
+	istringstream str(a);
 	T object;
-	//något
+
+	str >> object;
+
+	// TODO: Throw exception if wrong somehow?
 	return object;
 }
 
@@ -15,6 +23,11 @@ int main() {
 		int i = string_cast<int>("123");
 		double d = string_cast<double>("12.34");
 		Date date = string_cast<Date>("2015-01-10");
+
+		// Check if values are correct.
+		cout << i << "\n";
+		cout << d << "\n";
+		cout << date << "\n";
 	}
 	catch (invalid_argument& e) {
 		cout << "Error: " << e.what() << endl;
